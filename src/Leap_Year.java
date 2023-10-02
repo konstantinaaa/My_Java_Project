a726188b466e41d34d006ada2c3a8a41c589f25e
konstantinaaa <113425012+konstantinaaa@users.noreply.github.com>
2023-10-02 23:31:02 +0300

    Update Leap_Year.java

--- src/Leap_Year.java
+++ src/Leap_Year.java
@@ -2,7 +2,8 @@ import java.util.Scanner;
 
 public class Leap_Year {
     public static void main(String[] args) {
-        // Find a given year is a leap year
+        // Find a given year is a leap year. Extra leap day occurs in each year that is an integer multiple of 4 
+        // (except for years evenly divisible by 100, but not by 400).
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter a year number:");
